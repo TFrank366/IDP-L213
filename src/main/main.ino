bd6626b7ea9d7824101c0b2e3cc024f3d1cfc269
Thanassis Frank <thanassisfrank@gmail.com>
2022-02-20 14:08:25 +0000

    Initial work on robot setup

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,7 +1,42 @@
+// main.cpp
+// the main program file for the arduino
+const int m1Pin = 0;
+const int m2Pin = 1;
+const int oLedPin = 2;
+const int rLedPin = 3;
+const int gLedPin = 4;
+
+struct Motor {
+  int pin;
+  int currSpeed = 0;
+};
+
+struct Led {
+  int pin;
+};
+
+
 void setup() {
-  // put your setup code here, to run once:
+  // set the correct pins to output
+  pinMode(m1Pin, OUTPUT);
+  pinMode(m2Pin, OUTPUT);
+  // create motor structs
+  Motor m1{m1Pin};
+  Motor m2{m2Pin};
+  //create led structs
+}
+
+// supply motor
+void setMotorSpeed(Motor m, int s) {
 
 }
+void activeMotors(){
+  // active led flashing if it's not already
+}
+void stopMotors() {
+  // stop led flashing
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
