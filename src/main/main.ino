699df20d9aa2f29676710bdefdb6be517e144aa2
Thanassis Frank <thanassisfrank@gmail.com>
2022-02-21 11:29:01 +0000

    Initial motor function

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,15 +1,9 @@
-#include <Adafruit_CircuitPlayground.h>
-#include <Adafruit_Circuit_Playground.h>
-
 // main.cpp
 // the main program file for the arduino
 #include <Servo.h>;
 #include <Wire.h>;
 #include <Adafruit_MotorShield.h>;
-#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-int m1Pin = 0;
-int m2Pin = 1;
 int oLedPin = 2;
 int rLedPin = 3;
 int gLedPin = 4;
@@ -26,29 +20,25 @@ struct Led {
 };
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *m1 = AFMS.getMotor(1);
+Adafruit_DCMotor *m2 = AFMS.getMotor(2);
 
 // creat output control structs
-Motor m1;
-Motor m2;
 Led oLed;
+
+bool motorsActive = false;
 
 void setup() {
   // set the correct pins to output
-  pinMode(m1Pin, OUTPUT);
-  pinMode(m2Pin, OUTPUT);
-  // init motor structs
-  m1 = {m1Pin, 0};
-  m2 = {m2Pin, 0};
   //init led structs
   pinMode(oLedPin, OUTPUT);
   oLed = {.pin = oLedPin};
 
-  AFMS.begin()
-  myMotor->setSpeed(50);
-  myMotor->run(FORWARD);
-  delay(2000);
-  myMotor->setSpeed(0);
+  AFMS.begin();
+  m1->setSpeed(200);
+  m1->run(FORWARD);
+  m2->setSpeed(200);
+  m2->run(FORWARD);
 //  // create servo motor
 //  Servo grabberServo
 //  grabberServo.attach(servoPin)
@@ -57,30 +47,50 @@ void setup() {
 
 // supply motor
 
-void activeMotors(){
+void activateMotors(){
   // active led flashing if it's not already
+  motorsActive = true;
+  m1->run(FORWARD);
+  m2->run(FORWARD);
+  std::cout<<"motors started";
 }
+
 // set the speed
 void setMotorSpeed(Motor m, int s) {
+  
 }
 void stopMotors() {
   // stop led flashing
+  m1->run(RELEASE);
+  m2->run(RELEASE);
+  motorsActive = false;
+  digitalWrite(oLed.pin, false);
+  oLed.state = false;
 }
 
 unsigned long previousMillis = 0;
 unsigned long interval = 500;
+unsigned long elapsedTime = 0;
+
+
+void activateMotors();
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
   unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(oLed.pin, !oLed.state);
-    oLed.state = !oLed.state;
+  if (currentMillis > 5000) {
+    void stopMotors();
+  }
+  
+  
+  if (motorsActive) {
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+  
+      // set the LED with the ledState of the variable:
+      digitalWrite(oLed.pin, !oLed.state);
+      oLed.state = !oLed.state;
+    }
   }
 }
