99b495638314ef93b6f3e8c1eac1b555783d2b2c
Thanassis Frank <82391599+TFrank366@users.noreply.github.com>
2022-02-21 12:48:53 +0000

    Delete test file

--- test_file/test_file.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-
-// testing pushing, committing, pulling etc :)}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
