b90415a88b1b5d02e42aec040565fac898fcdbaa
tl525 <tl525@cam.ac.uk>
2022-02-22 09:10:12 +0000

    Colour detection

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,59 +1,61 @@
-// main.cpp
-// the main program file for the arduino
-const int m1Pin = 0;
-const int m2Pin = 1;
-const int oLedPin = 2;
-const int rLedPin = 3;
-const int gLedPin = 4;
+// Colour detection code
+// LED pins have already been initialised to false (initially)
 
-struct Motor {
-  int pin;
-  int currSpeed = 0;
-};
+// within pin numbers setup loop
+const int csbPin = 9 // based on the actual physical pin number, csb is in fact a light detecting resistor (LDR) that will drop different voltage based on light levels
+const int csrPin = 10 // based on the actual physical pin number, same as above
 
-struct Led {
-  int pin;
-  bool state = false;
-};
+// if a blue object is detected, the blue filter won't pick up anything (blue light filtered out) but the red filter will, so the blue sensor must read true if a blue object is seen, 
+// and thus has a red filter in front of it, while the redsensor must read false and thus has a blue filter in front of it
 
+// initialise the colour sensors (under initalisation of optoswitches)
+pinMode(csrPin, INPUT) // blue filter in front
+pinMode(csbPin, INPUT) // red filter in front
 
-void setup() {
-  // set the correct pins to output
-  pinMode(m1Pin, OUTPUT);
-  pinMode(m2Pin, OUTPUT);
-  // create motor structs
-  Motor m1{m1Pin};
-  Motor m2{m2Pin};
-  //create led structs
-  Led oLed{oLedPin};
-}
+// colour_reader function is ONLY called when there is the white LED shining and the robot has stopped moving!
 
-// supply motor
-void setMotorSpeed(Motor m, int s) {
-
-}
-void activeMotors(){
-  // active led flashing if it's not already
-}
-void stopMotors() {
-  // stop led flashing
-}
-
-unsigned long previousMillis = 0;
-unsigned long interval = 500;
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void colour_reader {
   
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(oLed.pin, !oLed.state);
-    oLed.state = !oLed.state;
+  int blue_light = analogRead(csbPin); 
   
+  int red_light = analogRead(cbrPin);
+  
+  if (blue_light > 400 && red_light < 400) { // whatever corresponds to a high or low analogue value, 400 is placeholder value. Might have to change to an OR statement
+    digitalWrite(gled.pin, true);
+    delay(5200); // 5.2 seconds just to be safe
+    digitalWrite(gled.pin, false);
+  }
+  
+  else {
+    digitalWrite(rled.pin, true);
+    delay(5200);
+    digitalWrite(rled.pin, false);
+  }
 
 }
+
+// Reference code from the internet:
+//const int ledPin = 13;
+//const int ldrPin = A0;
+//void setup() {
+//  Serial.begin(9600);
+//  pinMode(ledPin, OUTPUT);
+//  pinMode(ldrPin, INPUT);
+//}
+//
+//void loop() {
+//  int ldrStatus = analogRead(ldrPin);
+//
+//  if (ldrStatus <= 400)
+//  {
+//    digitalWrite(ledPin, HIGH);
+//    Serial.print("Its Dark, Turn on the LED:");
+//    Serial.println(ldrStatus);
+//
+//  }
+//  else
+//  {
+//    digitalWrite(ledPin, LOW);
+//    Serial.print("Its Bright, Turn off the LED:");
+//    Serial.println(ldrStatus);
+//  }
