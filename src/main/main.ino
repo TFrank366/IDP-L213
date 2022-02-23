9f2432cca41184177fdb89444db483e000bc77ee
Thanassis Frank <thanassisfrank@gmail.com>
2022-02-23 09:58:33 +0000

    Simple functioning line follower

--- src/main/main.ino
+++ src/main/main.ino
@@ -9,7 +9,8 @@
 const int oLedPin =   5;
 const int rLedPin =   3;
 const int gLedPin =   4;
-const int servoPin =  9;
+const int servo1Pin =  9;
+const int servo2Pin = 10;
 const int os1Pin =    0;  // analog pin #
 const int os2Pin =    1;  // analog pin #
 const int os3Pin =    2;  // analog pin #
@@ -89,7 +90,7 @@ void setup() {
   setMotorsStraight(fSpeed);
 
   // init the servo
-  servo.attach(servoPin);
+  servo.attach(servo1Pin);
 
   //start moving forward
   startMotors();
@@ -100,6 +101,7 @@ void setMotorsStraight(int mSpeed) {
   currSpeed = mSpeed;
   m1->setSpeed(mSpeed);
   m2->setSpeed(mSpeed);
+  //Serial.println("motors same");
 }
 
 void startMotors(){
@@ -107,7 +109,7 @@ void startMotors(){
   motorsActive = true;
   m1->run(FORWARD);
   m2->run(FORWARD);
-  Serial.println("motors started");
+  //Serial.println("motors started");
 }
 
 void stopMotors() {
@@ -117,7 +119,7 @@ void stopMotors() {
   motorsActive = false;
   digitalWrite(oLed.pin, false);
   oLed.state = false;
-  Serial.println("motors stopped");
+  //Serial.println("motors stopped");
 }
 
 // 0 is black 1 is white
@@ -143,23 +145,24 @@ int getColsVal(Sensor s1, Sensor s2, Sensor s3) {
 TurnCommand getTurnCommand(int colsVal){
     unsigned long nowMillis = millis();
     TurnCommand turn;
-    Serial.println(colsVal);
+    // Serial.println(colsVal);
     switch (colsVal) {
       // line only on right sensor
       // have to turn right
       case 1:
       case 3:  
         Serial.println("right");
-        turn = {RIGHT, true, 20, nowMillis, 1000};
+        turn = {RIGHT, true, 50, nowMillis, 100};
         break;
        // line only on left sensor
        // have to turn left
        case 4:
        case 6:
         Serial.println("left");
-        turn = {LEFT, true, 20, nowMillis, 1000};
+        turn = {LEFT, true, 50, nowMillis, 100};
         break;
        default:
+       turn.active = false;
         break;
     }
     return turn;
@@ -173,11 +176,11 @@ void startTurn(TurnCommand turn) {
   switch(turn.dir) {
     case LEFT:
       m2->setSpeed(int(currSpeed + turn.amount/2));
-      m1->setSpeed(int(currSpeed - turn.amount/2));
+      m1->setSpeed(max(int(currSpeed - turn.amount/2), 0)); // set to 0 if its too low TEMP
       break;
     case RIGHT:
       m2->setSpeed(int(currSpeed - turn.amount/2));
-      m1->setSpeed(int(currSpeed + turn.amount/2));
+      m1->setSpeed(max(int(currSpeed + turn.amount/2), 0)); // set to 0 if its too low TEMP
       break;
   }
 }
@@ -199,25 +202,31 @@ void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
 
+  servo.write(min(180, int(currentMillis/100)));
+
   
-//  // if the timer of the current turn command has elapsed, deacivate it
-//  if (currentMillis > turn.start + turn.duration && turn.active) {
-//    // set the active flag to off
-//    turn.active = false;
-//    // stop turning and move straight
-//    //setMotorsStraight(fSpeed);
-//  }
-//  
-//  // if the robot is currently turning, dont worry about this
-//  if (!turn.active){
-//    int colsVal = getColsVal(os1, os2, os3);
-//    //Serial.println(colsVal);
-//    //Serial.println(colsVal);//std::bitset<3>(colsVal))
-//    turn = getTurnCommand(colsVal);
-//    turn.active = true;
-//    //startTurn(turn);
-//  }
-  printVals(os1, os2, os3);
+  // if the timer of the current turn command has elapsed, deacivate it
+  if (currentMillis > turn.start + turn.duration && turn.active) {
+    // set the active flag to off
+    turn.active = false;
+    // stop turning and move straight
+    setMotorsStraight(fSpeed);
+    //Serial.println("stopped turn");
+  }
+  
+  // if the robot is currently turning, dont worry about this
+  if (!turn.active){
+    int colsVal = getColsVal(os1, os2, os3);
+    //Serial.println(colsVal);
+    //Serial.println(colsVal);//std::bitset<3>(colsVal))
+    turn = getTurnCommand(colsVal);
+    if (turn.active) {
+      startTurn(turn);
+    }
+    
+    printVals(os1, os2, os3);
+  }
+  
 
 
   // led flashes if the motors are active
