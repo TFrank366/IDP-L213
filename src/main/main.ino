a347c24615b2002ce98d0cd7af32eb9889700c9d
tl525 <tl525@cam.ac.uk>
2022-02-26 20:45:03 +0000

    Code cleanup
    Change the names of variables to allow them to be read immediately (left, right, distance etc) and added comments for final documentation

--- src/main/main.ino
+++ src/main/main.ino
@@ -6,25 +6,29 @@
 #include "logger.h"
 #include "movement.h"
 
-
 // pin for each component =======================================================================
 // digital pins
 // port 2 is borked
-const int oLedPin =         5;
-const int rLedPin =         3;
-const int gLedPin =         4;
+
+const int oLedPin =         5; // orange LED (movement)
+const int rLedPin =         3; // red LED (detection)
+const int gLedPin =         4; // green LED (detection)
 const int servo1Pin =       9;
 const int servo2Pin =      10;
+
 // analogue pins
+
 // line sensor
-const int os1Pin =         A2;  // analogue pin #
-const int os2Pin =         A1;  // analogue pin #
-const int os3Pin =         A0;  // analogue pin #
+const int rightsensorPin =        A2;  
+const int leftsensorPin =         A0;  
+
 // colour sensor
-const int bLDRPin =        A3;  // Blue colour LDR voltage (goes down with more light)        TBD
-const int rLDRPin =        A4;  // Red colour LDR voltage                                     TBD
-const int os4Pin =         A5;  // OPB704 Voltage (goes down with decreasing distance)        TBD
-const int wLedPin =         6;  // pin that the LED is attached to              TBD
+const int bLDRPin =        A3;  // Blue colour LDR voltage (goes down with more light)     
+const int rLDRPin =        A4;  // Red colour LDR voltage (goes down with more light)
+
+// distance sensor
+const int distsensorPin =         A5;  // OPB704 Voltage (goes down with decreasing distance)       
+          
 // ==============================================================================================
 const int fSpeed =        200; // motor speed for general movement
 // ==============================================================================================
@@ -35,11 +39,6 @@ enum Color {BLUE, RED};
 
 bool MOTORSREVERSED = false;
 bool robotStopped = true;
-
-//struct Motor {
-//  int pin;
-//  int currSpeed;
-//};
 
 struct Sensor {
   int pin;
@@ -54,8 +53,8 @@ struct Led {
 Logger l((unsigned long)0, USB);
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *m1 = AFMS.getMotor(1);
-Adafruit_DCMotor *m2 = AFMS.getMotor(2);
+Adafruit_DCMotor *leftwheel = AFMS.getMotor(1); 
+Adafruit_DCMotor *rightwheel = AFMS.getMotor(2); 
 
 Servo servo;
 
@@ -63,22 +62,21 @@ Servo servo;
 Led oLed;
 Led rLed;
 Led gLed;
-Led wLed;
 
 //create sensor structs 
+
 //optoswitches
-Sensor os1;
-Sensor os2;
-Sensor os3;
-Sensor os4;
+Sensor rightsensor;
+Sensor leftsensor;
+Sensor distsensor;
+
 //LDRs
 Sensor rLDR;
 Sensor bLDR;
 
-
 // global flag to keep track of if the motors are running to know when to flash oLed
 bool motorsActive = false;
-//int currSpeed = 0; // keep a track of the current speed
+
 // create object for handling the different movement regimes
 // handles line following
 Movement::FollowLine lineFollower(fSpeed, 50, (unsigned long)100);
@@ -95,7 +93,7 @@ void setup() {
   digitalWrite(NINA_RESETN, LOW);
   SerialNina.begin(115200);
 
-  //SerialNina.write(SerialNina.read());
+  // machine startup acknowledgement
   Serial.println("hello world");
 
   //init led structs
@@ -105,18 +103,14 @@ void setup() {
   rLed = {.pin = rLedPin};
   pinMode(gLedPin, OUTPUT);
   gLed = {.pin = gLedPin};
-  pinMode(wLedPin, OUTPUT);
-  wLed = {.pin = wLedPin};
 
   // init the sensors
-  pinMode(os1Pin, INPUT);
-  os1.pin = os1Pin;
-  pinMode(os2Pin, INPUT);
-  os2.pin = os2Pin;
-  pinMode(os3Pin, INPUT);
-  os3.pin = os3Pin;
-  pinMode(os4Pin, INPUT);
-  os4.pin = os4Pin;
+  pinMode(rightsensorPin, INPUT);
+  rightsensor.pin = rightsensorPin;
+  pinMode(leftsensorPin, INPUT);
+  leftsensor.pin = leftsensorPin;
+  pinMode(distsensorPin, INPUT);
+  distsensor.pin = distsensorPin;
   pinMode(bLDRPin, INPUT);
   bLDR.pin = bLDRPin;
   pinMode(rLDRPin, INPUT);
@@ -127,27 +121,25 @@ void setup() {
   
 }
 
-
 // general function to apply a motorSetting struct onto the motors
 void setMotors(Movement::MotorSetting mSetting) {
-  //l.logln("motors set");
-  m1->setSpeed(mSetting.speeds[0]);
-  m2->setSpeed(mSetting.speeds[1]);
+  leftwheel->setSpeed(mSetting.speeds[0]);
+  rightwheel->setSpeed(mSetting.speeds[1]);
   
   // handle if the motors are reversed so forward -> backward
   if (!MOTORSREVERSED) {
-    m1->run(mSetting.directions[0]);
-    m2->run(mSetting.directions[1]);
+    leftwheel->run(mSetting.directions[0]);
+    rightwheel->run(mSetting.directions[1]);
   } else {
     if (mSetting.directions[0] == FORWARD) {
-      m1->run(BACKWARD);
+      leftwheel->run(BACKWARD);
     } else {
-      m1->run(FORWARD);
+      leftwheel->run(FORWARD);
     }
     if (mSetting.directions[1] == FORWARD) {
-      m2->run(BACKWARD);
+      rightwheel->run(BACKWARD);
     } else {
-      m2->run(FORWARD);
+      rightwheel->run(FORWARD);
     }
   }
   
@@ -160,51 +152,35 @@ void setMotors(Movement::MotorSetting mSetting) {
   }
 }
 
-// 0 is black 1 is white
-int lightToBit(int light) {
-  if (light < 75){
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-//                           4  2  1
-// returns an int with bits (s3 s2 s1)
-// line on left => 100 => 4
-int getLineVal(Sensor s1, Sensor s2, Sensor s3) {
+int getLineVal(Sensor a, Sensor b) {
   int lineVal = 0;
-  lineVal |= analogRead(s1.pin) < 25;
+  lineVal |= analogRead(a.pin) < 25; // both optoswitches apparently have different sensitivity to lighting
   delay(10);
-  lineVal |= lightToBit(analogRead(s2.pin)) << 1;
-  delay(10);
-  lineVal |= (analogRead(s3.pin) < 800) << 2;
+  lineVal |= (analogRead(b.pin) < 800) << 1; // hence the difference in threshold value
   return lineVal;
 }
 
 // gets the colour of what is in front of the colour sensor
-// won't be accurate unless depth sensor (os4) reads < 300
+// won't be accurate unless depth sensor (distsensor) reads < 300
 Color getColorVal(Sensor rLDR, Sensor bLDR) {
   int bVal = analogRead(bLDR.pin);
   int rVal = analogRead(rLDR.pin);
-  if (bVal < rVal - 200){
+  if (bVal < rVal - 200){ // offset from red LDR needs to be corrected to give equal values when not picking up any red or blue
     return BLUE;
   } else {
     return RED;  
   }
 }
 
-String getValsString(Sensor s1, Sensor s2, Sensor s3) {
-  int a  = analogRead(s3.pin);
-  delay(10);
-  int b  = analogRead(s2.pin);
+String getValsString(Sensor s1, Sensor s2) {
+  int a  = analogRead(s2.pin);
   delay(10);
   int c  = analogRead(s1.pin);
   delay(10);
-  return String(a) + " " + String(b) + " " + String(c);
+  return String(a) + " " + String(c);
 }
 
-// serial command reciever
+// serial command receiver
 String getSerialCommand() {
   String command = Serial.readString();
   command = command.substring(0, command.length());
@@ -269,8 +245,8 @@ void loop() {
   // the main movement code 
   if (robotStopped) {
     setMotors(stopped.getMotorSetting());
-    //l.logln(analogRead(os4.pin));
-    if (analogRead(os4.pin) < 800) {
+    //l.logln(analogRead(distsensor.pin));
+    if (analogRead(distsensor.pin) < 800) {
       delay(10);
       l.logln(String(analogRead(rLDR.pin)) + " " + String(analogRead(bLDR.pin)));
       delay(10);
@@ -278,23 +254,23 @@ void loop() {
       if (blockCol == BLUE) {
         l.logln("blue");
         digitalWrite(gLed.pin, true);
-        delay(100);
+        delay(100); // change to 5100 for actual!
         digitalWrite(gLed.pin, false);
       } else {
         l.logln("red");
         digitalWrite(rLed.pin, true);
-        delay(100);
+        delay(100); // change to 5100 for actual!
         digitalWrite(rLed.pin, false);
       }
     }
   } else {
-    //int lineVal = getLineVal(os1, os2, os3);
-    //l.logln(String(analogRead(os3.pin)) + " " + String(analogRead(os2.pin)) + " " + String(analogRead(os1.pin)));
-    //l.logln(getValsString(os1, os2, os3));
+    //int lineVal = getLineVal(rightsensor, os2, leftsensor);
+    //l.logln(String(analogRead(leftsensor.pin)) + " " + String(analogRead(os2.pin)) + " " + String(analogRead(rightsensor.pin)));
+    //l.logln(getValsString(rightsensor, leftsensor));
 //    delay(10);
-    int lineVal = getLineVal(os1, os2, os3);
+    int lineVal = getLineVal(rightsensor, leftsensor);
     //l.logln(lineVal);
-//    if (lightToBit(analogRead(os1.pin)) == 1 && lightToBit(analogRead(os3.pin)) == 1) {
+//    if (lightToBit(analogRead(rightsensor.pin)) == 1 && lightToBit(analogRead(leftsensor.pin)) == 1) {
 //      setMotors(stopped.getMotorSetting());
 //    } else {
 //      setMotors(forward.getMotorSetting());
--- src/main/movement.cpp
+++ src/main/movement.cpp
@@ -5,21 +5,22 @@
 
 using namespace Movement;
 
-// takes an input integer that represents state of the sensors as bits (s3 s3 s1) 
-// robot is setup m1---m2
-//                 \ ^ /
-//                  \o/
+// takes an input integer that represents state of the sensors as bits 
+
 MotorSetting FollowLine::getMotorSetting(int cols) {
   unsigned long nowMillis = millis();
   // create new motorsetting struct
+  
   MotorSetting mSetting;
-  // if both are on then stop regardless of whether a turn is in progress
-//  if (cols == 0b101 || cols == 0b111) {
-//    mSetting = {.speeds = {0, 0}, .directions = {FORWARD, FORWARD}};
-//    return mSetting;
-//  }
+
+// if both are on then stop regardless of whether a turn is in progress (has hit a junction)
+  if (cols == 0b11) {
+     mSetting = {.speeds = {0, 0}, .directions = {FORWARD, FORWARD}};
+     return mSetting;
+     }
+
   if (!turning || nowMillis - turnStart > turnDuration) {
-    // if there is not a turn taking place or it has elapsed
+    // if there is not a turn taking place or it has elapsed (to check if it needs to continue turning or go straight)
 
     //set the turning flag to false
     turning = false;
@@ -28,10 +29,10 @@ MotorSetting FollowLine::getMotorSetting(int cols) {
     int speedMinus = fSpeed - turnAmount/2;
     
     switch (cols) {
+      
       // line only on right sensor
       // have to turn right
-      case 0b001:
-      case 0b011:  
+      case 0b01: 
         mSetting = {.speeds = {speedPlus, abs(speedMinus)}, .directions = {FORWARD, FORWARD}};
         if (speedMinus < 0) {mSetting.directions[1] = BACKWARD;};
         turning = true;
@@ -41,8 +42,7 @@ MotorSetting FollowLine::getMotorSetting(int cols) {
         
        // line only on left sensor
        // have to turn left
-       case 0b100:
-       case 0b110:
+       case 0b10:
         mSetting = {.speeds = {abs(speedMinus), speedPlus}, .directions = {FORWARD, FORWARD}};
         if (speedMinus < 0) {mSetting.directions[0] = BACKWARD;};
         turning = true;
@@ -51,7 +51,7 @@ MotorSetting FollowLine::getMotorSetting(int cols) {
         break;
         
        default:
-        // go straight ahead
+        // go straight ahead ie 0b00
         mSetting = {.speeds = {fSpeed, fSpeed}, .directions = {FORWARD, FORWARD}};
         break;
     }
